2fc8d0b5a9f1dc1c45854aadd62a3bc179df849e
Arun Kumar R <aruntutter19@gmail.com>
2024-06-13 15:42:07 +0530

    floating navigation bar created

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
+import { FaHome, FaUser, FaFolderOpen, FaEnvelope } from "react-icons/fa";
 
 const Navbar = () => {
-  return <nav className="navbar">Navbar</nav>;
+  const [activeNavItem, setActiveNavItem] = useState("home");
+
+  const handleNavItemClick = (navItem) => {
+    setActiveNavItem(navItem);
+  };
+
+  return (
+    <nav className="navbar">
+      <ul className="navbar-nav">
+        <li className={`nav-item ${activeNavItem === "home" ? "active" : ""}`}>
+          <a
+            href="#home"
+            className="nav-link"
+            onClick={() => handleNavItemClick("home")}
+          >
+            <FaHome className="nav-icon" />
+          </a>
+        </li>
+        <li className={`nav-item ${activeNavItem === "about" ? "active" : ""}`}>
+          <a
+            href="#about"
+            className="nav-link"
+            onClick={() => handleNavItemClick("about")}
+          >
+            <FaUser className="nav-icon" />
+          </a>
+        </li>
+        <li
+          className={`nav-item ${
+            activeNavItem === "portfolio" ? "active" : ""
+          }`}
+        >
+          <a
+            href="#portfolio"
+            className="nav-link"
+            onClick={() => handleNavItemClick("portfolio")}
+          >
+            <FaFolderOpen className="nav-icon" />
+          </a>
+        </li>
+        <li
+          className={`nav-item ${activeNavItem === "contact" ? "active" : ""}`}
+        >
+          <a
+            href="#contact"
+            className="nav-link"
+            onClick={() => handleNavItemClick("contact")}
+          >
+            <FaEnvelope className="nav-icon" />
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
