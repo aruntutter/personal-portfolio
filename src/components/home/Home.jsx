0fc5ed993b40754b96d7a915d938464329418078
Arun Kumar R <aruntutter19@gmail.com>
2024-06-13 17:17:16 +0530

    home page created

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,8 +1,45 @@
 import React from "react";
 import "./Home.css";
+import { FaDownload } from "react-icons/fa";
+import ImageHome from "../../assets/home-img.jpg";
 
 const Home = () => {
-  return <section>Home</section>;
+  const handleDownload = () => {
+    window.location.href =
+      "https://drive.google.com/file/d/1y_sR7Fw1exg-7-Fhav6rucgeDS77xaVI/view?usp=sharing";
+  };
+
+  return (
+    <section className="home">
+      <div className="home-wrapper">
+        <div className="home-left">
+          <p className="home-p1">Hey there,</p>
+          <h1>
+            I'm Arun
+            <br />
+            Kumar R
+          </h1>
+          <div className="role-title">
+            <p className="home-p2">-- Frontend Developer</p>
+          </div>
+          <div className="role-des">
+            <p className="home-p3">
+              Front-End Web Developer | Actively Advancing Skills in Modern Web
+              Technologies | Pursuing Japanese - JLPT N5
+            </p>
+          </div>
+          <div className="btn">
+            <button onClick={handleDownload}>
+              Download Resume <FaDownload className="icon-download" />
+            </button>
+          </div>
+        </div>
+        <div className="home-right">
+          <img src={ImageHome} alt="man standing with laptop" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
