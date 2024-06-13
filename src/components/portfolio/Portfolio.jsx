14faf7879a7a17e6f615d79b62cb9001fcedd533
Arun Kumar R <aruntutter19@gmail.com>
2024-06-13 21:00:06 +0530

    portfolio section created

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,60 @@
 import React from "react";
 import "./Portfolio.css";
+import portfolioImg1 from "../../assets/portfolio-img1.png";
+import portfolioImg2 from "../../assets/portfolio-img2.png";
+import portfolioImg3 from "../../assets/portfolio-img3.png";
+
+const projectData = [
+  {
+    title: "E-Com Website",
+    imgSrc: portfolioImg1,
+    demoLink: "https://ez-buy.netlify.app/",
+    sourceLink: "https://github.com/aruntutter/react-ecommerce-crud-app",
+  },
+  {
+    title: "Blog Website",
+    imgSrc: portfolioImg2,
+    demoLink: "https://blogops.netlify.app/",
+    sourceLink: "https://github.com/aruntutter/react-firebase-blog-app",
+  },
+  {
+    title: "Movie Search",
+    imgSrc: portfolioImg3,
+    demoLink: "https://movie-search-quest.netlify.app/",
+    sourceLink:
+      "https://github.com/aruntutter/react-movie-search-with-api-integration",
+  },
+];
 
 const Portfolio = () => {
   return (
-    <section>
+    <section className="portfolio">
       <h2 className="h2">PORTFOLIO</h2>
+      <p className="p">
+        Welcome to my portfolio! Here, you will find a showcase of my frontend
+        development projects, demonstrating my expertise in HTML, CSS,
+        JavaScript, React, and Firebase. Each project reflects my commitment to
+        creating dynamic, user-friendly, and responsive web applications.
+        Explore the highlights of my work and see how I bring ideas to life
+        through code.
+      </p>
+
+      <div className="list-of-projects">
+        {projectData.map((project, index) => (
+          <div className="project" key={index}>
+            <img src={project.imgSrc} alt={project.title} />
+            <div className="project-overlay">
+              <a href={project.demoLink} className="project-link">
+                Live Demo
+              </a>
+              <a href={project.sourceLink} className="project-link">
+                Source Code
+              </a>
+            </div>
+            <p className="project-name">{project.title}</p>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
